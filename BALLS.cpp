81e74fff421183b4407778776362efd63dbc63c2
J-Wiggler <ConsumerOfWorlds@protonmail.com>
2022-10-18 16:00:16 -0400

    added division of message into blocks

--- BALLS.cpp
+++ BALLS.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 
 
-void doThing() {
-
+string doThing(string message) {
+  int blockSize = 4;
+  vector<vector<char>> blocks;
+  vector<char> tempBlock;
+  for (int i = 0; i < (int) message.length(); i++) {
+    tempBlock.push_back(message.at(i));
+    if (i % blockSize == 0) {
+      blocks.push_back(tempBlock);
+      vector<char> empty;
+      tempBlock = empty;
+    }
+  }
+  return message;
 }
 
 int main() {
-  return 0;
+  cout << "Enter a message >> " << "\n";
+  string msg;
+  cin >> msg;
+  cout << "\n" << doThing(msg) << "\n";
+  return 69;
 }
